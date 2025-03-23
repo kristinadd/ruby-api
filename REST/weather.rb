c86e8335073445058fc9b9113828d2e9845d8835
kristinadd <kristina@productboard.com>
2025-03-23 06:49:46 +0100

    REST

--- REST/weather.rb
+++ REST/weather.rb
@@ -1,10 +1,19 @@
+# Standard Ruby library modules are pre-installed Ruby code packages that come built-in with Ruby itself. 
+# You don’t need to install anything extra — you just use require to load them into your script.
+# no gem install needed
+
 require 'net/http'
 require 'json'
 require 'uri'
 
 class Weather
 # Define the endpoint
-url = URI("https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&current_weather=true")
+url = URI("https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&past_days=1&daily=temperature_2m_max")
+
+# forecast_days default to 7
+# So you are getting:
+# 1 day in the past (yesterday)
+# 7 days into the future
 
 # Send a GET request
 response = Net::HTTP.get(url)
@@ -12,7 +21,13 @@ response = Net::HTTP.get(url)
 # Parse the JSON response
 data = JSON.parse(response)
 
-# Print the current temperature
-puts "Current temperature in London: #{data['current_weather']['temperature']}°C"
+# turn strings into symbols
+# data = JSON.parse(response, symbolize_names: true)
 
+
+puts data
+
+# puts data[:current_weather][:temperature]
+# puts data["current_weather"]["temperature"]
+# puts data["current_weather"]["time"]
 end
